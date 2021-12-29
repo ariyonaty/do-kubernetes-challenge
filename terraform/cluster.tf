117ded41da73a32467168797a77568d6872d7381
ariyonaty <ariyonaty@users.noreply.github.com>
2021-12-28 20:32:44 -0800

    Include delete and destroy resources in README

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -16,64 +16,6 @@ resource "digitalocean_kubernetes_cluster" "kube_cluster" {
 }
 
 
-resource "digitalocean_firewall" "kube_firewall" {
-  name        = "k8s-firewall"
-  droplet_ids = [digitalocean_kubernetes_cluster.kube_cluster.node_pool[0].nodes[0].droplet_id]
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "80"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "443"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "31000"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-}
-
-# resource "digitalocean_loadbalancer" "ingress" {
-#   name   = "${var.cluster_name}-lb"
-#   region = var.region
-
-#   forwarding_rule {
-#     entry_port     = 8080
-#     entry_protocol = "http"
-
-#     target_port     = 5000
-#     target_protocol = "http"
-#   }
-
-#   # forwarding_rule {
-#   #   entry_port     = 443
-#   #   entry_protocol = "https"
-
-#   #   target_port     = 5000
-#   #   target_protocol = "https"
-#   # }
-
-#   healthcheck {
-#     port     = 22
-#     protocol = "tcp"
-#   }
-
-#   # droplet_tag = "node-pool-tag"
-
-#   droplet_ids = [digitalocean_kubernetes_cluster.kube_cluster.node_pool[0].nodes[0].droplet_id]
-
-# }
-
 output "cluster-id" {
   value = digitalocean_kubernetes_cluster.kube_cluster.id
 }
-
-# output "lb-ip" {
-#   value = digitalocean_loadbalancer.ingress.ip
-# }
